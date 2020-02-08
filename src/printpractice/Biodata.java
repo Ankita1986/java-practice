a634caf493afab73f6b2761e4e357d81cb877a30
Ankita1986 <ankitakaswala1@gmail.com>
2020-02-08 16:15:32 +0000

    Java practice project

---
+++ src/printpractice/Biodata.java
@@ -0,0 +1,12 @@
+package printpractice;
+
+public class Biodata {
+    public static void main(String[] args){
+        System.out.print("Ankita ");
+        System.out.println(" Kaswala");
+
+        System.out.println("5,Wood Farmroad");
+        System.out.println("HemelHempstead");
+        System.out.println("HP2 4LG");
+    }
+}
---
+++ src/printpractice/HelloWorld.java
@@ -0,0 +1,14 @@
+package printpractice;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("World");
+
+        System.out.print("Hello ");
+        System.out.print(" World");
+    }
+}
